717fa5165ab90da31e39a28ee4cc88ac5c4f6057
houdunwang <2300071698@qq.com>
2016-12-29 15:24:14 +0800

    no message

--- system/config/alipay.php
+++ system/config/alipay.php
@@ -1,24 +1,44 @@
 <?php
-
 return [
+	/**
+	 * 合作身份者id
+	 * 以2088开头的16位纯数字
+	 */
 	'partner'       => '',
-	//合作身份者id，以2088开头的16位纯数字
+
+	/**
+	 * 安全检验码
+	 * 以数字和字母组成的32位字符
+	 */
 	'key'           => '',
-	//安全检验码，以数字和字母组成的32位字符
+
+	/**
+	 * 收款支付宝账号
+	 * 一般情况下收款账号就是签约账号
+	 */
 	'seller_email'  => '',
-	//收款支付宝账号，一般情况下收款账号就是签约账号
-	'payment_type'  => 1,
+
 	//支付类型
-	'notify_url'    => '',
+	'payment_type'  => 1,
+
 	//服务器异步通知页面路径
-	'return_url'    => '',
+	'notify_url'    => '',
+
 	//页面跳转同步通知页面路径
-	'sign_type'     => strtoupper( 'MD5' ),
+	'return_url'    => '',
+
 	//签名方式 不需修改
-	'input_charset' => strtolower( 'utf-8' ),
+	'sign_type'     => strtoupper( 'MD5' ),
+
 	//字符编码格式
-	'cacert'        => getcwd() . '\\cacert.pem',
-	//访问模式,根据自己的服务器是否支持ssl访问，若支持请选择https；若不支持请选择http
-	'transport'     => 'http',
+	'input_charset' => strtolower( 'utf-8' ),
+
 	//ca证书路径地址，用于curl中ssl校验
+	'cacert'        => getcwd() . '\\cacert.pem',
+
+	/**
+	 * 访问模式根据自己的服务器是否支持ssl访问
+	 * 若支持请选择https；若不支持请选择http
+	 */
+	'transport'     => 'http',
 ];
