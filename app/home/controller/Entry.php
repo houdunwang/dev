ab6e4f9abc64cf8b272e0bca748aec6fcf582aa6
houdunwang <2300071698@qq.com>
2017-01-06 17:03:04 +0800

    no message

--- app/home/controller/Entry.php
+++ app/home/controller/Entry.php
@@ -4,11 +4,13 @@ use houdunwang\model\Model;
 use system\model\News;
 
 class Entry {
-	public function index( News $m ) {
-		return view();
+	public function index() {
+		return view( '', 5 );
 	}
+
 	public function post( News $m ) {
-		View::with(['a'=>3]);
+		View::with( [ 'a' => 3 ] );
+
 		return view();
 	}
 }
--- app/home/view/entry/index.php
+++ app/home/view/entry/index.php
@@ -9,5 +9,6 @@
 <h1 class="text-muted text-center" style="margin-top:200px;font-size:80px;">HDPHP 3 </h1>
 <h4 class="text-muted text-center">服务化/组件化/模块化的未来框架产品</h4>
 <h6 class="text-muted text-center">hdphp: {{HDPHP_VERSION}} framework: {{FRAMEWORK_VERSION}}</h6>
+<?php echo time();?>
 </body>
 </html>
